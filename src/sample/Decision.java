ae8fadb0a06100dfda67af8034ea412d9dfb1ceb
PastelilloAzul <49769083+PastelilloAzul@users.noreply.github.com>
2019-06-06 13:24:22 -0700

    comments added to decision

--- src/sample/Decision.java
+++ src/sample/Decision.java
@@ -1,34 +1,29 @@
 /*
-James Kahle
-
 The Decision class is used to keep track of the description and
 effects of each decision
-
-desciption text should be 1 sentence long, begining with number that
-corresponds to the number of the decision
-
 */
 package sample;
 import java.util.*;
 
 public class Decision {
-   //String for text
+   //text is the decision or choice description
    private String text;
    
    //effects are stored in an array for ease of passing to the play class
    private Effect[] effects; 
-
+   
+   //sets the fields to the parameter values
    public Decision(String text, Effect[] effects ) {
       this.text = text;
       this.effects = effects;
    }
+   //returns the decision string
    public String getTxt() {
       return text;
    }
-   
+   //returns the effect arrays
    public Effect[] getEffs() {
       return effects;  
    }   
    
-   //we may want a print method but that can be done from the oneGame class
 }
