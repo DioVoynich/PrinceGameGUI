044d76c4c4b6dea4df9ec53021548ee7c83488d1
PastelilloAzul <49769083+PastelilloAzul@users.noreply.github.com>
2019-06-06 13:23:34 -0700

    comments added to event file

--- src/sample/Event.java
+++ src/sample/Event.java
@@ -1,33 +1,20 @@
 /*
-James Kahle
-
 The Event class is used to keep track of the description and
 decisions that make up an event
-
-desciption text should be around 3 to 4 sentences long, begining with an 
-address to the player "M'lord" and ending with asking what the players command is
 */
 package sample;
 import java.util.*;
-
+   //event holds the event discription, if a year has passed and the decisions that you can take
 public class Event {
-   //does this event take 1 year of game time
-   //public boolean isChain;
    private boolean year;
-  
-  //string text for event description
    private String text;
-   
-   //decisions are stored in a decision array
    private Decision[] decisions;
    private String title;
    public Event left;
    public Event right;
    public boolean isLoss;
    
-   //notice how the constructor requires the decision array to already be formed
-   //we may want to change this order later
-   
+   //this is the constructor sets the fields to the parameter values
    public Event(boolean year, String text, Decision[] decisions, String title) {
 
       this.year = year;
@@ -36,27 +23,21 @@ public class Event {
       this.title = title;
       
    }
-   
+   //returns true if increases year else false
    public boolean increasesYear() {
       return year;
    }
-   
+   //returns the event description text
    public String getTxt() {
       return text;
    }
-
+   // returns the title text
    public String getTitle() {
        return title;
    }
-   
+   //returns the array with the decisions
    public Decision[] getDecs() {
       return decisions;
    }
 
-
-   
-
-   
-   //additional methods will almost certaintly be needed for things
-   //event trees but this is just the most basic design
 }
